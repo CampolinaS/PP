c927994282d0d317185dc673469a45ee45e25aa3
Pedro  Márcio <pedromos16@gmail.com>
2020-06-19 23:17:38 +0000

    Primeiras implementações

--- main.c
+++ main.c
@@ -4,18 +4,46 @@
 #include <math.h>
 #include <string.h>
 
-/*
-struct Talunos
+struct Tcliente
 {
+  int codigo,telefone,dataNasci;
+  char nome[50],endereco[50];
+}
+typedef struct Tcliente cliente;
 
-    int matricula;
-    char nome[50];
-    float nota1,nota2,nota3;
-};
-typedef struct Talunos aluno;
-*/
+struct Tfuncionario{
+int codigo,telefone;
+char nome[50],funcao[50],tipo[10];
+float salario;
+}
+typedef struct Tfuncionario funcionario
+
+struct Tfornecedor{
+int codigo,telefone;
+char nome[50],produtoFornecido[50];
+}
+typedef struct Tfornecedor fornecedor
+
+struct Tfesta
+{
+  // Data = Int? dataDia, dataMes, dataAno
+  //Data = string? 12/12/12
+int codigoFesta,codigoCliente,qntConvidados,data,horarioInicio,horarioFim;
+char tema[50],diaSemana[20];
+}
+typedef struct Tfesta festa
+
+struct Tcontrato
+{
+int numContrato,codigoFesta;
+char formaPagamento[50],status[50];
+float valorTotal,desconto,valorFinal;
+}
+typedef struct Tcontrato contrato
+
 int main() {
   setlocale(LC_ALL, "portuguese");
   printf("Olá mundo\n");
   return 0;
 }
+int 
