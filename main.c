f1c3d0dff3badf04d39992004cc18b4aeed3fda3
Pedro  Márcio <pedromos16@gmail.com>
2020-06-19 22:39:44 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/*
+struct Talunos
+{
+
+    int matricula;
+    char nome[50];
+    float nota1,nota2,nota3;
+};
+typedef struct Talunos aluno;
+*/
+int main() {
+  setlocale(LC_ALL, "portuguese");
+  printf("Olá mundo\n");
+  return 0;
+}
